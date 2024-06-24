c5a4309aac8e14d11adf55d508235f28fcff471a
Peer <peerfichman@gmail.com>
2024-06-24 12:02:19 +0300

    sercer with scapy

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 python3 -m venv vpn_env
 source vpn_env/bin/activate
-pip install pydivert pyOpenSSL pycryptodome
+pip install pydivert pyOpenSSL pycryptodome scapy
 python3 vpn_server.py
 deactivate
 exit
--- vpn_server.py
+++ vpn_server.py
@@ -1,15 +1,15 @@
 import socket
 from Crypto.Cipher import AES
-import base64
-import os
 from dotenv import load_dotenv
+import os
+from scapy.all import IP
 
 load_dotenv()
 
 ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY').encode('raw_unicode_escape').decode('unicode_escape').encode(
     "raw_unicode_escape")
-SERVER_IP = '10.0.2.15'
-SERVER_PORT = 9999
+SERVER_IP = os.getenv('SERVER_IP')
+SERVER_PORT = os.getenv('SERVER_PORT')
 
 
 def create_server_socket():
@@ -24,21 +24,31 @@ def handle_client(client_socket, addr):
 
     # Receive encrypted data from client
     nonce = client_socket.recv(16)
+    tag = client_socket.recv(16)
     encrypted_data = client_socket.recv(4096)
+
     cipher = AES.new(ENCRYPTION_KEY, AES.MODE_EAX, nonce=nonce)
-    decrypted_data = cipher.decrypt(encrypted_data)
-    print(f"Received from Clien: {decrypted_data}")
+    decrypted_data = cipher.decrypt_and_verify(encrypted_data, tag)
+
+    print(f"Received from Client: {decrypted_data}")
+
+    # Extract destination IP address using scapy
+    ip_packet = IP(decrypted_data)
+    destination_ip = ip_packet.dst
+    destination_port = ip_packet.dport
+    print(f"Extracted Destination IP: {destination_ip} and Port: {destination_port}")
+
     # Forward the decrypted data
     forward_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # forward_socket.connect(("destination_server_ip", 80))  # Forward to actual destination -- remove quate
-    # forward_socket.sendall(decrypted_data) --  rmove quate
+    forward_socket.connect((destination_ip, destination_port))
+    forward_socket.sendall(decrypted_data)
 
     # Receive response and send back to client
-    # response_data = forward_socket.recv(4096) --remove quate
-    # encrypted_response = cipher.encrypt(response_data) #-- remove quate
+    response_data = forward_socket.recv(4096)
     cipher = AES.new(ENCRYPTION_KEY, AES.MODE_EAX)
-    encrypted_response = cipher.encrypt(b'hello back')  # remove line
-    client_socket.sendall(cipher.nonce + encrypted_response)
+    encrypted_response, tag = cipher.encrypt_and_digest(response_data)
+    client_socket.sendall(cipher.nonce + tag + encrypted_response)
+
     forward_socket.close()
     client_socket.close()
 
